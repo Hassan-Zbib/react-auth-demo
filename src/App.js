4957f5498d60c807cd26da9002b766eafd8a47ca
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-02-28 06:23:44 +0200

    MUI and router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
 function App() {
   return (
-    <div>
-      <h1>Hellooo</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Container>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/Signup' element={<Signup />} />
+            <Route path='/Dashboard' element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </Container>
+    </>
   );
 }
 
