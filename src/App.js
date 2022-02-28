dfe248b6ad4d9ead01e5b46e9ecc72571d95295e
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-02-28 07:39:10 +0200

    added mui navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline"
+import Container from "@mui/material/Container"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Dashboard from './pages/Dashboard'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import Dashboard from "./pages/Dashboard"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Container>
-        <Router>
+      <Router>
+        <CssBaseline />
+        <Container>
+          <Navbar />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/Login' element={<Login />} />
-            <Route path='/Signup' element={<Signup />} />
-            <Route path='/Dashboard' element={<Dashboard />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
           </Routes>
-        </Router>
-      </Container>
+        </Container>
+      </Router>
     </>
   );
 }
