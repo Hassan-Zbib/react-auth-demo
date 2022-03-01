aa9f41e05534ae698804f845d4fcc07249407d89
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-03-01 07:14:02 +0200

    secured dashboard route

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+
 const Dashboard = () => {
+  const navigate = useNavigate()
+
+  const { user } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if(!user) {
+      navigate('/')
+    }
+  }, [user, navigate])
+
   return <div>Dashboard</div>;
 };
 export default Dashboard;
